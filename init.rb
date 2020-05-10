86661dd600c3fd55ab7d7172915b51768770ece8
cincarnato <cristian.incarnato@sondeos.com.ar>
2020-05-10 10:16:20 -0300

    Se agrega manejo de emails al plugin

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :disable_menus do
   name 'Disable Activity'
   author 'Cristian Incarnato'
   version '1.0.0'
-  description 'Disable overview and activity tab'
+  description 'Disable overview and activity tab & mail change behavior'
 
   Redmine::MenuManager.map(:project_menu).delete(:activity)
   Redmine::MenuManager.map(:project_menu).delete(:overview)
